405183ecce06d9bdc6b0adc905deaf8e107bf53e
Place4you <sadiraja500@gmail.com>
2024-07-21 12:57:29 +0500

    Event Binding and Filtering

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -11,4 +11,18 @@ import { Component } from "@angular/core";
 export class AppNavbar{
     isActive : boolean= false;
 
+    onClick(){
+        console.log('Clicked');
+        
+    }
+
+    onKeypress($event:any){
+        console.log($event.key);
+        
+    }
+
+    onEnterPressed(){
+        console.log('Enter Pressed');
+        
+    }
 }
