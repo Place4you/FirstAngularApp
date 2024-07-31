b4e23ea19ea92de19a8cdfa1b4f4f13ccc83ad1f
Place4you <sadiraja500@gmail.com>
2024-07-31 13:07:19 +0500

    conditional template and reactive forms

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { TemplateComponent } from './template/template.component';
+import { ReactiveComponent } from './reactive/reactive.component';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [ TemplateComponent,ReactiveComponent],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.css'
 })
 export class FormsComponent {
 
+  templateform: boolean= false;
+  reactiveform: boolean= false;
+
+  template(){
+    this.templateform = true;
+  }
+
+  reactive(){
+    this.reactiveform= true;
+  }
 }
