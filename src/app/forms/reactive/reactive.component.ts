904cc2fdc137e94ab721f23491c916550e579079
Place4you <sadiraja500@gmail.com>
2024-07-31 13:34:56 +0500

    feat

--- src/app/forms/reactive/reactive.component.ts
+++ src/app/forms/reactive/reactive.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive.component.html',
   styleUrl: './reactive.component.css'
 })
 export class ReactiveComponent {
 
+  studentForm: FormGroup = new FormGroup({
+    name: new FormControl(""),
+    email: new FormControl(""),
+    password: new FormControl(""),
+    isAcceptedTerms: new FormControl(false),
+  })
+
+  formValues:any;
+  saveForm(){
+    this.formValues = this.studentForm;
+  }
 }
