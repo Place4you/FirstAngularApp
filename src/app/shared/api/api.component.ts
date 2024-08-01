36574f6a5dbd045da0ee143ca9626b0361840079
Place4you <sadiraja500@gmail.com>
2024-08-01 16:59:02 +0500

    new

--- src/app/shared/api/api.component.ts
+++ src/app/shared/api/api.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { GetapiComponent } from './getapi/getapi.component';
+import { PostapiComponent } from './postapi/postapi.component';
 
 @Component({
   selector: 'app-api',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, GetapiComponent, PostapiComponent],
   templateUrl: './api.component.html',
   styleUrl: './api.component.css'
 })
 export class ApiComponent {
-
+  get: boolean=false;
+  post: boolean=false;
+    
+  callGetApi(){
+    this.get= true;
+  }
+  callPostApi(){
+    this.post= true;
+  }
 }
--- src/app/shared/api/getapi/getapi.component.ts
+++ src/app/shared/api/getapi/getapi.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-getapi',
   standalone: true,
-  imports: [],
+  imports: [HttpClient],
   templateUrl: './getapi.component.html',
   styleUrl: './getapi.component.css'
 })
 export class GetapiComponent {
+
+  http = inject (HttpClient);
+  list:any[]=[];
+  getusers(){
+    this.http.get("https://jsonplaceholder.typicode.com/users").subscribe((res:any)=>{
+      this.list = res.value;
+    })
+  }
 
 }
