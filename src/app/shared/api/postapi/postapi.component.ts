3002f1eafd361b284d9df190ef10f31399df6c88
Place4you <sadiraja500@gmail.com>
2024-08-02 12:19:43 +0500

    new

--- src/app/shared/api/postapi/postapi.component.ts
+++ src/app/shared/api/postapi/postapi.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-postapi',
@@ -9,4 +10,27 @@ import { Component } from '@angular/core';
 })
 export class PostapiComponent {
 
+  apiurl: string = "https://projectapi.gerasim.in/api/Complaint/AddNewDepartment";
+  errorMessage: string | null = null;
+  successMessage: string | null = null;
+  data = {
+    departmentId: 5,
+    departmentName: "bonga",
+    departmentLogo: "check"
+  };
+
+  constructor(private http: HttpClient) {}
+
+  addDepartment() {
+    this.http.post(this.apiurl, this.data).subscribe(
+      (res: any) => {
+        this.successMessage = "Department added successfully.";
+        this.errorMessage = null; // Clear the error message if the request is successful
+      },
+      (error) => {
+        this.errorMessage = "Failed to add department. Please try again later.";
+        this.successMessage = null; // Clear the success message in case of error
+      }
+    );
+  }
 }
