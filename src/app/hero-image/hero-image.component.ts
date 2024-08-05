85164868657b3d1cc5ac31a11ad17338d1f5da15
Place4you <sadiraja500@gmail.com>
2024-08-05 15:14:35 +0500

    viewchild

--- src/app/hero-image/hero-image.component.ts
+++ src/app/hero-image/hero-image.component.ts
@@ -1,6 +1,6 @@
 import { ViewchildComponent } from './../viewchild/viewchild.component';
 import { NgClass, NgStyle, NgSwitch } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-hero-image',
@@ -9,9 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './hero-image.component.html',
   styleUrl: './hero-image.component.css'
 })
-export class HeroImageComponent {
+export class HeroImageComponent implements AfterViewInit {
 
   person: string = 'member';
   test: boolean= false;
   testr: boolean= true;
+  msg:any;
+
+  @ViewChild(ViewchildComponent) view?: ViewchildComponent;
+
+  ngAfterViewInit(){
+   this.msg=this.view?.msg;
+   console.log(this.msg);
+   
+}
 }
--- src/app/viewchild/viewchild.component.ts
+++ src/app/viewchild/viewchild.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-viewchild',
   standalone: true,
@@ -7,6 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './viewchild.component.html',
   styleUrl: './viewchild.component.css'
 })
-export class ViewchildComponent {
+export class ViewchildComponent implements AfterViewInit{
+  @ViewChild('txt') textbox?:ElementRef;
 
+  msg:string= ("message from child componenet named ViewChild");
+
+  ngAfterViewInit(): void {
+    const value = this.textbox?.nativeElement.value;
+    console.log(value);
+    
+  }
 }
