e1beffd5225cfb6b530d89fd36a3e6410618cb9e
Place4you <sadiraja500@gmail.com>
2024-08-05 14:36:58 +0500

    view child

--- src/app/hero-image/hero-image.component.ts
+++ src/app/hero-image/hero-image.component.ts
@@ -1,10 +1,11 @@
+import { ViewchildComponent } from './../viewchild/viewchild.component';
 import { NgClass, NgStyle, NgSwitch } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero-image',
   standalone: true,
-  imports: [NgStyle,NgClass],
+  imports: [NgStyle, NgClass, ViewchildComponent],
   templateUrl: './hero-image.component.html',
   styleUrl: './hero-image.component.css'
 })
