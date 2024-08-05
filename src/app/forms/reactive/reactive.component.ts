04f78cd4e16257a1e801391b861c46dc68aa1076
Place4you <sadiraja500@gmail.com>
2024-08-05 10:04:00 +0500

    reactive form check

--- src/app/forms/reactive/reactive.component.ts
+++ src/app/forms/reactive/reactive.component.ts
@@ -1,26 +1,29 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   standalone: true,
   imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './reactive.component.html',
-  styleUrl: './reactive.component.css'
+  styleUrls: ['./reactive.component.css']
 })
 export class ReactiveComponent {
 
   studentForm: FormGroup = new FormGroup({
-    name: new FormControl(""),
-    email: new FormControl(""),
-    password: new FormControl(""),
-    isAcceptedTerms: new FormControl(false),
-  })
+    name: new FormControl("", [Validators.required, Validators.minLength(4)]),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required, Validators.minLength(6)]),
+    isAcceptedTerms: new FormControl(false, [Validators.requiredTrue]),
+  });
 
-  formValues:any;
-  saveForm(){
-    this.formValues = this.studentForm.value;
-    
+  formValues: any;
+
+  saveForm() {
+    debugger;
+    if (this.studentForm.valid) {
+      this.formValues = this.studentForm.value;
+    }
   }
 }
