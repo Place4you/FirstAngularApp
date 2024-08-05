9cffdb7fec55b8203de9325d0cc5dec16f271bea
Place4you <sadiraja500@gmail.com>
2024-08-05 11:09:06 +0500

    samll

---
+++ src/app/life-cycle-events/life-cycle-events.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-life-cycle-events',
+  standalone: true,
+  imports: [],
+  templateUrl: './life-cycle-events.component.html',
+  styleUrl: './life-cycle-events.component.css'
+})
+export class LifeCycleEventsComponent {
+
+}
