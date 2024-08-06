32bbeabfc8a88df09de3f7df2cdab6db9943800b
Place4you <sadiraja500@gmail.com>
2024-08-06 12:27:23 +0500

    Login Routing Succesfull

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  userObj: any = {
+    username: '',
+    password: ''
+  }
+  router = inject(Router)
+  onLogin(){
+    
+    if(this.userObj.username =='admin' && this.userObj.password == '1234')
+    {
+      alert('Login Successful!');
+      this.router.navigateByUrl('add-header')
+    }
+    else{
+      alert('Invalid Details.')
+    }
+  }
+
+
 
 }
