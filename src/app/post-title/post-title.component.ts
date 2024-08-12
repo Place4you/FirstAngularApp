736743041f1bf85156cf6472cf57792f402d3333
Place4you <sadiraja500@gmail.com>
2024-08-12 14:33:43 +0500

    signal learning

--- src/app/post-title/post-title.component.ts
+++ src/app/post-title/post-title.component.ts
@@ -1,27 +1,32 @@
-import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, signal } from '@angular/core';
 
 @Component({
   selector: 'app-post-title',
   standalone: true,
   imports: [],
   templateUrl: './post-title.component.html',
-  styleUrl: './post-title.component.css'
+  styleUrl: './post-title.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PostTitleComponent {
 
   constructor(){
+    const firstname =  this.firstName();
     setTimeout(() => {
-      this.firstName= signal("after Change");
+      this.firstName.set("after Change Signal");
+      this.lastName = "after Change Variable";
     }, 5000);
   }
+
+  // Signal Testing
+  firstName= signal("before Change Signal");
+  lastName= "before Change Variable";
+
+
 
   @Input() toChild: string = '';  
   // childData : string = 'This data is coming from child using @output & Event Emiter';
 
-
-  // Signal Testing
-  firstName= signal("before Change");
-  lastName= "before Change";
 
 
   @Output() myData = new EventEmitter<string>();
