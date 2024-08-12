11efaa6e523ded4ee3216521c302acdcacf2c679
Place4you <sadiraja500@gmail.com>
2024-08-12 13:02:59 +0500

    constructor for form

---
+++ src/app/core/models/class/DepartmentPost.ts
@@ -0,0 +1,12 @@
+export class DepartmentPost{
+    departmentId?: number;
+    departmentLogo: string;
+    departmentName: string;
+
+    constructor(){
+    this.departmentId = 0;
+    this.departmentLogo = '';
+    this.departmentName = '';
+    }
+
+}
